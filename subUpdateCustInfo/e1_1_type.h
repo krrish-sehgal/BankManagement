5ba45daab1a36f436a5684c66bf02cd96b27f951
KrrishSehgal <krrishsehgal03@gmail.com>
2023-11-10 17:01:31 +0530

    krrish

--- subUpdateCustInfo/e1_1_type.h
+++ subUpdateCustInfo/e1_1_type.h
@@ -2,9 +2,6 @@
 #include<stdio.h>
 #include "../CustomerStructs/s1_Customer.h"
 
-   
-
-
 void changeType(struct Customer *customer) {
     printf("Select the account type you want to modify to:\n");
     printf("1. Current Account\n");
@@ -18,13 +15,10 @@ void changeType(struct Customer *customer) {
     switch (choice) {
         case 1:
             customer->accDet.accountType="Current Account";
-            
             break;
         case 2:
             customer->accDet.accountType="savings Account";
             break;
-        
-
         default:
             printf("Error: Invalid choice. Please select a valid option.\n");
            
