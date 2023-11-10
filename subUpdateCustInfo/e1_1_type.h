5fce65c8de3ba759c7d987b6480fcb29536912f8
Abhijeet Sirohi <abhijeetsirohiajso@gmail.com>
2023-11-10 12:29:24 +0530

    check

--- subUpdateCustInfo/e1_1_type.h
+++ subUpdateCustInfo/e1_1_type.h
@@ -2,7 +2,37 @@
 #include<stdio.h>
 #include "../CustomerStructs/s1_Customer.h"
 
-void changeType(struct Customer * customer){
-    printf("enter the Type you want modify the account to"); // change it to 1,2,3 options like savings vageh 
-    scanf("%s",customer->accDet.accountType );
-}   
+   
+
+
+void changeType(struct Customer *customer) {
+    printf("Select the account type you want to modify to:\n");
+    printf("1. Current Account\n");
+    printf("2. Savings Account\n");
+    // Add more options as needed
+
+    int choice;
+    printf("Enter your choice (1, 2): ");
+
+    // Adding error handling for user input
+    if (scanf("%d", &choice) != 1) {
+        fprintf(stderr, "Error: Invalid input for account type selection.\n");
+        // Handle error as needed
+        return;
+    }
+
+    switch (choice) {
+        case 1:
+            snprintf(customer->accDet.accountType, sizeof(customer->accDet.accountType), "Current Account");
+            break;
+        case 2:
+            snprintf(customer->accDet.accountType, sizeof(customer->accDet.accountType), "Savings Account");
+            break;
+        // Add more cases for additional options
+
+        default:
+            fprintf(stderr, "Error: Invalid choice. Please select a valid option.\n");
+            // Handle error as needed
+            break;
+    }
+}
